77c908a2eb88f5889f4f68622c1c4c7ccd13e714
Brianj61479 <115123910+Brianj61479@users.noreply.github.com>
2023-05-05 23:37:06 -0400

    first commit

---
+++ src/components/Movies.jsx
@@ -0,0 +1,132 @@
+/** @format */
+const movies = [
+  {
+    title: "Parasite",
+    director: "Bong Joon-ho",
+    year: 2019,
+    length: 132,
+    country: "South Korea",
+  },
+  {
+    title: "Empire Strikes Back",
+    director: "Irvin Kershner",
+    year: 1980,
+    length: 124,
+    country: "USA",
+  },
+  {
+    title: "Das Boot",
+    director: "Wolfgang Petersen",
+    year: 1981,
+    length: 149,
+    country: "Germany",
+  },
+  {
+    title: "Temple of Doom",
+    director: "Steven Spielberg",
+    year: 1984,
+    length: 118,
+    country: "USA",
+  },
+  {
+    title: "M",
+    director: "Fritz Lang",
+    year: 1931,
+    length: 117,
+    country: "Germany",
+  },
+  {
+    title: "Metropolis",
+    director: "Fritz Lang",
+    year: 1927,
+    length: 148,
+    country: "Germany",
+  },
+  {
+    title: "Godfather",
+    director: "Francis Ford Coppola",
+    year: 1972,
+    length: 175,
+    country: "USA",
+  },
+  {
+    title: "Godfather, Part II",
+    director: "Francis Ford Coppola",
+    year: 1974,
+    length: 202,
+    country: "USA",
+  },
+  {
+    title: "Loaded Weapon, Part I",
+    director: "Gene Quintano",
+    year: 1993,
+    length: 84,
+    country: "USA",
+  },
+  {
+    title: "Spaceballs",
+    director: "Mel Brooks",
+    year: 1987,
+    length: 96,
+    country: "USA",
+  },
+  {
+    title: "Dracula, Dead and Loving It",
+    director: "Mel Brooks",
+    year: 1995,
+    length: 88,
+    country: "USA",
+  },
+  {
+    title: "Major League",
+    director: "David S. Ward",
+    year: 1989,
+    length: 107,
+    country: "USA",
+  },
+  {
+    title: "Airplane!",
+    director: "Jim Abrahams, David Zucker, Jerry Zucker",
+    year: 1980,
+    length: 88,
+    country: "USA",
+  },
+  {
+    title: "Airplane II: The Sequel",
+    director: "Ken Finkleman",
+    year: 1982,
+    length: 85,
+    country: "USA",
+  },
+];
+
+const Movies = ({ movies }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title (year)</th>
+          <th>Director(s)</th>
+          <th>Length</th>
+          <th>Country</th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map(({ title, year, director, length, country }) => {
+          return (
+            <tr key={`${title}${year}`}>
+              <td>
+                {title} ({year})
+              </td>
+              <td>{director}</td>
+              <td>{length}</td>
+              <td>{country}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Movies;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,76 @@
+/** @format */
+
+import { useState, useEffect } from "react";
+
+const SearchBar = ({
+  search,
+  setSearch,
+  maxLength,
+  setMaxLength,
+
+  Descend,
+  Ascend,
+}) => {
+  const [setOrder, setSwitch] = useState(false);
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const maxLengthHandler = (e) => {
+    setMaxLength(e.target.value);
+  };
+
+  const switchOrder = () => {
+    if (setOrder == false) {
+      Descend();
+      setSwitch(true);
+    } else {
+      Ascend();
+      setSwitch(false);
+    }
+  };
+
+  return (
+    <div>
+      <center>
+        <div>
+          <div>
+            <label htmlFor='search'>Search</label>
+          </div>
+          <input
+            className='peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0'
+            type='text'
+            name='search'
+            onChange={searchHandler}
+          />
+        </div>
+
+        <div>
+          <label htmlFor='max-length'>Maximum Length</label>
+          <div>
+            <input
+              className='peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0'
+              type='number'
+              name='max-length'
+              onChange={maxLengthHandler}
+              value={maxLength}
+            />
+          </div>
+        </div>
+
+        <button
+          className='pb-50 inline-block rounded-full bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 
+          active:bg-primary-700 
+          active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] 
+          dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]'
+          onClick={switchOrder}
+          type='button'
+        >
+          Ascend/Descend
+        </button>
+      </center>
+    </div>
+  );
+};
+
+export default SearchBar;
